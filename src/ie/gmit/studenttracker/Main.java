57ca9e96ede207802860560d19dd560e7f77e3ed
Daniel Cregg <daniel.cregg@gmit.ie>
2021-03-26 15:47:14 +0000

    add new class and updated main

--- src/ie/gmit/studenttracker/Main.java
+++ src/ie/gmit/studenttracker/Main.java
@@ -1,5 +1,8 @@
 package ie.gmit.studenttracker;
 
+//Import the Scanner class to read user input
+import java.util.Scanner;
+
 /**
  * This is the Main class. It contains the main method.
  */
@@ -8,9 +11,73 @@ public class Main {
 	// This is the main method. It contains a print statement.
 	public static void main(String[] args) {
 
-		// This is a print statement.
-		//System.out.println("This is the main method. I am inside the Main calss.");
-		System.out.println("Hello");
+		// Instantiate Scanner class so we can start reading user input
+		Scanner userInput = new Scanner(System.in);
+		// Used to store user Menu option selection
+		int userSelection = 0;
+	
+		
+		ArrayController ac = new ArrayController(5);
+
+		// Create infinite while loop to display Menu after every command
+		while (true) {
+			// Display Menu to console
+			System.out.println("");
+			System.out.println("##################################");
+			System.out.println("#      Student Application v0.9  #");
+			System.out.println("##################################");
+			System.out.println("(1) Add a Student.");
+			System.out.println("(2) Delete a Student.");
+			System.out.println("(3) Show total Number of Students.");
+			System.out.println("(4) Search for a Student by ID.");
+			System.out.println("(5) Quit.");
+			System.out.println("Select an option from 1 to 5 and press Enter>");
+			
+			// Store user Menu option selection
+			userSelection = userInput.nextInt();
+			
+			// Execute option
+			if (userSelection == 1) { // Add Student
+				
+				System.out.println("Enter Student ID>");
+				String studentID = userInput.next();
+				System.out.println("Enter age of Student>");
+				short age = userInput.nextShort();
+				
+				boolean result = ac.addStudent(studentID, age);
+				
+				System.out.println(result);
+			} else if (userSelection == 2) { // Delete	
+				System.out.println("Enter Student ID>");
+				String studentID = userInput.next();
+				
+				boolean result = ac.deleteStudent(studentID);
+				
+				System.out.println(result);
+				
+			} else if (userSelection == 3) { // Count	
+				
+				System.out.println(ac.countStudents());
+				
+			} else if (userSelection == 4) { // Search	
+				System.out.println("Enter Student ID>");
+				String studentID = userInput.next();
+				
+				Student newStudent = ac.searchByStudentID(studentID);
+				
+				if (newStudent == null) {
+					System.out.println("Student NOT found");
+				} else {
+					System.out.println("Student found!");
+				}
+				
+			} else if (userSelection == 5) { // quit
+			
+				break;
+			}
+			
+
+		}
 
 	} // End main method
 } // End Main Class
